52ab2b3099588066f9f0c233f4904a835732543e
Mahmoud ELshora <67161371+elshora@users.noreply.github.com>
2023-10-30 21:14:25 +0200

    update package and add types

---
+++ types.d.ts
@@ -0,0 +1,48 @@
+type EVENT = {
+  _id: string;
+  internalId: string;
+  title: string;
+  description: string;
+  link: string;
+  organizer: string;
+  location: string;
+  date: number;
+  externalLinks: string[];
+  sessions: Types.ObjectId[];
+  tags: string[];
+};
+
+type SESSION = {
+  internalId: string;
+  title: string;
+  description: string;
+  event: Types.ObjectId;
+  experts: Types.ObjectId[];
+};
+
+type EXPERT = {
+  _id: string;
+  internalId: string;
+  firstName: string;
+  lastName: string;
+  title: string;
+  organization: string;
+  bio: string;
+  imgLink: string;
+  sessions: Types.ObjectId[];
+  events: Types.ObjectId[];
+  gender: "male" | "female";
+  nationality: string;
+  age: number;
+  email: string;
+  externalLinks: string[];
+  tags: string[];
+};
+
+type TABLEEXPERT = {
+  name: HTMLElement;
+  title: string;
+  organization: string;
+  bio: string;
+  img: HTMLElement;
+};
