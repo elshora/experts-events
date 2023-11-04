c73b6de8e8c9272a1ee85006326a59a9deea5651
Mahmoud ELshora <67161371+elshora@users.noreply.github.com>
2023-11-04 17:18:38 +0200

    fix events route

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -2,23 +2,23 @@ import EventsModel from "@/models/events";
 import { connectDb } from "@/util/db";
 import moment from "moment";
 import { NextRequest, NextResponse } from "next/server";
-type queryType = {
-  date?: object;
-};
+// type queryType = {
+//   date?: object;
+// };
 
 export async function GET<T extends Request | NextRequest>(req: T) {
   try {
     await connectDb();
-    const currentDate = moment(); // Current date and time
-    const url = new URL(req.url);
-    const searchParam = new URLSearchParams(url.search); // Get the searchParam from the query string
-    const query: queryType = {};
-    if (searchParam.get("period") === "upcoming") {
-      query.date = { $gte: currentDate.toDate() };
-    } else if (searchParam.get("period") === "old") {
-      query.date = { $lt: currentDate.toDate() };
-    }
-    const events = await EventsModel.find(query);
+    // const currentDate = moment(); // Current date and time
+    // const url = new URL(req.url);
+    // const searchParam = new URLSearchParams(url.search); // Get the searchParam from the query string
+    // const query: queryType = {};
+    // if (searchParam.get("period") === "upcoming") {
+    //   query.date = { $gte: currentDate.toDate() };
+    // } else if (searchParam.get("period") === "old") {
+    //   query.date = { $lt: currentDate.toDate() };
+    // }
+    const events = await EventsModel.find();
     return NextResponse.json({ events }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Some thing went Wrong!" });
